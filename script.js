ea6d819eedb476399726244c2494b9c88f28c88d
alejandroh9800 <98057819+alejandroh9800@users.noreply.github.com>
2022-02-17 12:37:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.onload = (event) => {
+    /*
 
 if ( document.URL.includes("index") ) {
 
     document.getElementById("home").style.borderBottom= '2px #3882f6 solid';
 }
+
+*/
 
 if ( document.URL.includes("About-Me") ) {
 
