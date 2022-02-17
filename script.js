4b474f7797f0ec16c3f79b3acbf25de4bcbba188
alejandroh9800 <98057819+alejandroh9800@users.noreply.github.com>
2022-02-17 12:56:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ if ( on_index = true && document.URL.includes("Contact" || "Projects" || "About-
      document.getElementById("home").style.borderBottom= 'none';
 }
     
-else if ( on_index = true){
+else ( on_index = true){
     
     document.getElementById("home").style.borderBottom= '2px #3882f6 solid';
 }      
